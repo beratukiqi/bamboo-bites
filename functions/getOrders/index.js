b291c1a2b2f5ae49d69cfd9291a82cb9b39fae36
fabiolapoblete <113706603+fabiolapoblete@users.noreply.github.com>
2023-11-08 11:15:04 +0100

    functionality for posting orders

---
+++ functions/getOrders/index.js
@@ -0,0 +1,22 @@
+import { ScanCommand } from "@aws-sdk/lib-dynamodb";
+import { docClient } from "../../services/client";
+import { sendResponse } from "../../responses";
+
+exports.handler = async (event) => {
+  try {
+    const command = new ScanCommand({
+      TableName: "bambooBites-orders",
+    });
+
+    const response = await docClient.send(command);
+    const orders = response.Items;
+    return sendResponse(200, { success: true, orders });
+  } catch (error) {
+    console.error("error occurred:", error);
+    return sendResponse(
+      500,
+      { success: false, message: "Unable to get orders" },
+      error
+    );
+  }
+};
---
+++ functions/putOrder/index.js
@@ -0,0 +1,36 @@
+import { sendResponse } from "../../responses/index";
+import { docClient } from "../../services/client";
+import { PutCommand } from "@aws-sdk/lib-dynamodb";
+import { nanoid } from "nanoid";
+
+exports.handler = async (event) => {
+  const { order } = JSON.parse(event.body);
+
+  try {
+    // const orderNr = nanoid(); This will generate a string do we want string och number?
+    const orderNr = 3;
+    // let order = [
+    //   { id: 1, name: "Something good", price: 10, desc: "so so good" },
+    //   { id: 1, name: "Something good", price: 10, desc: "so so good" },
+    // ];
+
+    const command = new PutCommand({
+      TableName: "bambooBites-orders",
+      Item: {
+        orderNr: orderNr,
+        order: order,
+      },
+    });
+
+    const response = await docClient.send(command);
+    return sendResponse(200, {
+      success: true,
+      message: "A new order has been added",
+    });
+  } catch (error) {
+    return sendResponse(500, {
+      success: false,
+      message: "Could not add order",
+    });
+  }
+};
