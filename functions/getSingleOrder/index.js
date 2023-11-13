2c510f2fbbb406423b7d34f427579dbc876859bb
fabiolapoblete <113706603+fabiolapoblete@users.noreply.github.com>
2023-11-13 13:04:15 +0100

    GetCommand

--- functions/getSingleOrder/index.js
+++ functions/getSingleOrder/index.js
@@ -1,4 +1,42 @@
-import { QueryCommand } from "@aws-sdk/lib-dynamodb";
+// import { QueryCommand } from "@aws-sdk/lib-dynamodb";
+// import { docClient } from "../../services/client";
+// import { sendResponse } from "../../responses";
+
+// exports.handler = async (event) => {
+//   const orderNr = event.pathParameters.orderNr;
+
+//   try {
+//     const command = new QueryCommand({
+//       TableName: "bambooBites-orders",
+//       KeyConditionExpression: "#onr = :orderNr",
+//       ExpressionAttributeNames: {
+//         "#onr": "orderNr",
+//       },
+//       ExpressionAttributeValues: {
+//         ":orderNr": parseInt(orderNr),
+//       },
+//     });
+
+//     const response = await docClient.send(command);
+
+//     return sendResponse(200, {
+//       success: true,
+//       message: "Filtered order",
+//       order: response.Items,
+//       orderNr: orderNr,
+//     });
+//   } catch (error) {
+//     console.error("Error fetching order:", error);
+//     return sendResponse(500, {
+//       success: false,
+//       message: "Not able to get order",
+//       error: error.message,
+//       response: response,
+//       orderNr: orderNr,
+//     });
+//   }
+// };
+import { GetCommand } from "@aws-sdk/lib-dynamodb";
 import { docClient } from "../../services/client";
 import { sendResponse } from "../../responses";
 
@@ -6,23 +44,27 @@ exports.handler = async (event) => {
   const orderNr = event.pathParameters.orderNr;
 
   try {
-    const command = new QueryCommand({
+    const command = new GetCommand({
       TableName: "bambooBites-orders",
-      KeyConditionExpression: "#onr = :orderNr",
-      ExpressionAttributeNames: {
-        "#onr": "orderNr",
-      },
-      ExpressionAttributeValues: {
-        ":orderNr": parseInt(orderNr),
+      Key: {
+        orderNr: parseInt(orderNr),
       },
     });
 
     const response = await docClient.send(command);
 
+    if (!response.Item) {
+      return sendResponse(404, {
+        success: false,
+        message: "Order not found",
+        orderNr: orderNr,
+      });
+    }
+
     return sendResponse(200, {
       success: true,
-      message: "Filtered order",
-      order: response.Items,
+      message: "Fetched order",
+      order: response.Item,
       orderNr: orderNr,
     });
   } catch (error) {
@@ -31,7 +73,6 @@ exports.handler = async (event) => {
       success: false,
       message: "Not able to get order",
       error: error.message,
-      response: response,
       orderNr: orderNr,
     });
   }
