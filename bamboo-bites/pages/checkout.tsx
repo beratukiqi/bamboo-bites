b2641120ddf164a70e5e85452271374c2085400d
beratukiqi <berat.u@live.se>
2023-11-14 10:25:24 +0100

    Checkout Page done

--- bamboo-bites/pages/checkout.tsx
+++ bamboo-bites/pages/checkout.tsx
@@ -3,42 +3,42 @@ import PageColumn from "@/components/PageColumn";
 import PageHeader from "@/components/PageHeader";
 import PageWrapper from "@/components/PageWrapper";
 import AppContext from "@/context/AppContext";
-import { useRouter } from "next/router";
-import { useContext, useEffect, useState } from "react";
+import router from "next/router";
+import { useContext } from "react";
 
-interface OrderItem {
-  id: number;
-  title: string;
-  price: number;
-  image: string;
-  quantity: number;
-}
-
-const Cart = () => {
+const Checkout = () => {
   const { cart } = useContext(AppContext);
-  const router = useRouter();
 
-  const toCheckout = async () => {
-    router.push(`/checkout`);
+  const sendOrder = async () => {
+    const res = await fetch(
+      "https://x1keilhp1a.execute-api.eu-north-1.amazonaws.com/api/putOrder",
+      {
+        method: "POST",
+        body: JSON.stringify(cart),
+      }
+    );
+
+    const data = await res.json();
+    console.log("DATA/RES", data);
+    console.log("SENT ORDER", cart);
+
+    router.push(`/order/${data.orderNr}`);
   };
-
-  useEffect(() => {
-    console.log("CART", cart);
-  }, [cart]);
 
   return (
     <main>
       <PageWrapper column>
         <PageHeader
-          title="Cart"
+          title="Checkout"
           img="https://i.ibb.co/GMzvf0P/noodles-bowl-720x1024-72px-1.png"
         />
-        <PageColumn title="Your cart">
+
+        <PageColumn title="Please review your order">
           <section className="order-item__wrapper">
             {cart && cart.map((item) => <CartItem key={item.id} item={item} />)}
           </section>
-          <button onClick={toCheckout} style={{ padding: "2rem" }}>
-            GO TO CHECKOUT
+          <button onClick={sendOrder} style={{ padding: "2rem" }}>
+            SEND ORDER
           </button>
         </PageColumn>
       </PageWrapper>
@@ -46,4 +46,4 @@ const Cart = () => {
   );
 };
 
-export default Cart;
+export default Checkout;
