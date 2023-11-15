32bc5d8c4a6bd22cf74e354cbe05ad3deae17de9
beratukiqi <berat.u@live.se>
2023-11-15 10:53:39 +0100

    Static page cleanup + added helper components

---
+++ bamboo-bites/components/helpers/AboutInfo.tsx
@@ -0,0 +1,22 @@
+const AboutInfo = () => {
+  return (
+    <>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+        voluptates, voluptatem, quia, quibusdam quos voluptatum dicta doloribus
+        quod consequatur voluptate doloremque. Quisquam voluptates, voluptatem,
+        quia, quibusdam quos voluptatum dicta doloribus quod consequatur
+        voluptate doloremque.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+        voluptates, voluptatem, quia, quibusdam quos voluptatum dicta doloribus
+        quod consequatur voluptate doloremque. Quisquam voluptates, voluptatem,
+        quia, quibusdam quos voluptatum dicta doloribus quod consequatur
+        voluptate doloremque.
+      </p>
+    </>
+  );
+};
+
+export default AboutInfo;
--- bamboo-bites/pages/index.tsx
+++ bamboo-bites/pages/index.tsx
@@ -1,18 +1,15 @@
-import Button from "@/components/Button";
 import PageHeader from "@/components/PageHeader";
 import PageWrapper from "@/components/PageWrapper";
 
 const Home = () => {
   return (
-    <main>
-      <PageWrapper>
-        <PageHeader
-          landingpage
-          title="Bamboo Bites"
-          img="https://i.ibb.co/GMzvf0P/noodles-bowl-720x1024-72px-1.png"
-        ></PageHeader>
-      </PageWrapper>
-    </main>
+    <PageWrapper>
+      <PageHeader
+        landingpage
+        title="Bamboo Bites"
+        img="https://i.ibb.co/GMzvf0P/noodles-bowl-720x1024-72px-1.png"
+      />
+    </PageWrapper>
   );
 };
 
