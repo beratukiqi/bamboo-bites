4321c30dd28fc6349edd7ac19009bf0cc0ede96a
LisaRydCarlsson <lisa@rydcarlsson.se>
2023-11-22 14:46:24 +0100

    More CSS addtions

--- bamboo-bites/components/helpers/ContactInfo.tsx
+++ bamboo-bites/components/helpers/ContactInfo.tsx
@@ -1,33 +1,67 @@
+import ContactForm from "../ContactForm";
+import { SvgIcons } from "../SvgIcons";
+
+const socials = [
+	{
+		path: "/facebook",
+		Icon: SvgIcons.Facebook,
+	},
+	{
+		path: "/instagram",
+		Icon: SvgIcons.Instagram,
+	},
+	{
+		path: "/snapchat",
+		Icon: SvgIcons.Snapchat,
+	},
+	{
+		path: "/tiktok",
+		Icon: SvgIcons.TikTok,
+	},
+];
+
 const ContactInfo = () => {
-  return (
-    <section className="contact-container">
-      <h2 className="h2-contact">WHERE TO FIND US</h2>
-        <section className="address-map-container">
-          <section className="address">
-            <h3>BAMBOO BITES</h3>
-            <br />
-            <p>123 SUSHI STREET</p>
-            <p>BROOKLYN, NY 11234 </p>
-            <br />
-            <p>PHONE ICON + (555) 123-4567</p>
-          </section>
-          <section className="map">
-          <img className="about-img" src="https://bamboo-bites-bucket.s3.eu-north-1.amazonaws.com/mobile/map.png" alt="Bamboo Bites Street Map" />
-          </section>
-        </section>
-        <h2 className="h2-contact">OPENING HOURS <br /> EVERY DAY 11 AM - 11 PM</h2>
-        <section className="contact-form">
-          <h3 className="h3-contact">Reach Out, We're Friendly!</h3>
-          <p>CONTACT FORM</p>
-        </section>
-        <section className="social">
-          <h3 className="h3-contact">Taste the Buzz on Social!</h3>
-          <br />
-          <p>SoMe ICONS HERE</p>
-          
-        </section>
-    </section>
-  );
+	return (
+		<section className="contact-container">
+			<h2 className="h2-contact">WHERE TO FIND US</h2>
+			<section className="address-map-container">
+				<section className="address">
+					<h3>BAMBOO BITES</h3>
+					<br />
+					<p>123 SUSHI STREET</p>
+					<p>BROOKLYN, NY 11234 </p>
+					<br />
+					<section className="phone">
+						{SvgIcons.Phone}
+						<p> (555) 123-4567</p>
+					</section>
+				</section>
+				<section className="map">
+					<img
+						className="about-img"
+						src="https://bamboo-bites-bucket.s3.eu-north-1.amazonaws.com/mobile/map.png"
+						alt="Bamboo Bites Street Map"
+					/>
+				</section>
+			</section>
+			<h2 className="h2-contact">
+				OPENING HOURS <br /> EVERY DAY 11 AM - 11 PM
+			</h2>
+			<section className="contact-form-container">
+				<h3 className="h3-contact">Reach Out, We're Friendly!</h3>
+				<ContactForm />
+			</section>
+			<section className="social">
+				<h3>Taste the Buzz on Social!</h3>
+				<br />
+				<section className="social-container">
+					{socials.map((item) => (
+						<a href={item.path}> {item.Icon} </a>
+					))}
+				</section>
+			</section>
+		</section>
+	);
 };
 
 export default ContactInfo;
--- bamboo-bites/pages/about.tsx
+++ bamboo-bites/pages/about.tsx
@@ -4,17 +4,17 @@ import PageWrapper from "@/components/PageWrapper";
 import AboutInfo from "@/components/helpers/AboutInfo";
 
 const About = () => {
-  return (
-    <PageWrapper column>
-      <PageHeader
-        title="About"
-        img="https://bamboo-bites-bucket.s3.eu-north-1.amazonaws.com/desktop/about_desktop_720x1024.png"
-      />
-      <PageColumn title="">
-        <AboutInfo />
-      </PageColumn>
-    </PageWrapper>
-  );
+	return (
+		<PageWrapper column>
+			<PageHeader
+				title="About"
+				img="https://bamboo-bites-bucket.s3.eu-north-1.amazonaws.com/desktop/about_desktop_720x1024.png"
+			/>
+			<PageColumn title="">
+				<AboutInfo />
+			</PageColumn>
+		</PageWrapper>
+	);
 };
 
 export default About;
