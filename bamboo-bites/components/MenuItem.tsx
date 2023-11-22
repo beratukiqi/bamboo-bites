2c581b74704eb3924b7423f81d206d604481da6c
LisaRydCarlsson <lisa@rydcarlsson.se>
2023-11-22 14:40:49 +0100

    More css additions

--- bamboo-bites/components/MenuItem.tsx
+++ bamboo-bites/components/MenuItem.tsx
@@ -2,46 +2,46 @@ import { useState } from "react";
 import Modal from "./Modal";
 
 interface MenuItemProps {
-  food: {
-    id: string;
-    item: string;
-    price: number;
-    desc: string;
-    imgUrl: string;
-  };
+	food: {
+		id: string;
+		item: string;
+		price: number;
+		desc: string;
+		imgUrl: string;
+	};
 }
 
 const MenuItem = ({ food }: MenuItemProps) => {
-  const { id, item, price, imgUrl, desc } = food;
-  const [modalOpen, setModalOpen] = useState(false);
+	const { id, item, price, imgUrl, desc } = food;
+	const [modalOpen, setModalOpen] = useState(false);
 
-  return (
-    <>
-      <article
-        onClick={() => setModalOpen(true)}
-        className="menu-item__container"
-      >
-        <img
-          src={imgUrl}
-          alt={`Top view image of the dish ${item}`}
-          className="menu-item__image"
-        />
-        <header className="menu-item__text">
-          <h2 className="h2-menu-item">{item}</h2>
-          <p>Dive into the world of Ambient Sushi, where...</p>
-        </header>
-        <span className="menu-item__price">
-          <b>$</b>
-          {price}
-        </span>
-      </article>
-      <Modal
-        isOpen={modalOpen}
-        closeModal={() => setModalOpen(false)}
-        food={food}
-      />
-    </>
-  );
+	return (
+		<>
+			<article
+				onClick={() => setModalOpen(true)}
+				className="menu-item__container"
+			>
+				<img
+					src={imgUrl}
+					alt={`Top view image of the dish ${item}`}
+					className="menu-item__image"
+				/>
+				<header className="menu-item__text">
+					<h2 className="h2-menu-item">{item}</h2>
+					<p className="menu-descr">{desc}</p>
+				</header>
+				<span className="menu-item__price">
+					<b>$</b>
+					{price}
+				</span>
+			</article>
+			<Modal
+				isOpen={modalOpen}
+				closeModal={() => setModalOpen(false)}
+				food={food}
+			/>
+		</>
+	);
 };
 
 export default MenuItem;
