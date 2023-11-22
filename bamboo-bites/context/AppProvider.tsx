a8479544aad818d7c39d003d46543198920b61a3
fabiolapoblete <113706603+fabiolapoblete@users.noreply.github.com>
2023-11-22 10:45:31 +0100

    Added OrderDetails contex to the AppProvider

--- bamboo-bites/context/AppProvider.tsx
+++ bamboo-bites/context/AppProvider.tsx
@@ -19,12 +19,17 @@ interface CartItemProps {
   tweaks?: string[];
 }
 
+interface OrderDetails {
+  deliveryMethod: string;
+}
+
 const AppProvider = ({ children }: any) => {
   const [menuItems, setMenuItems] = useState<MenuItemProps[]>([]);
   const [cart, setCart] = useState<CartItemProps[]>([]);
+  const [orderDetails, setOrderDetails] = useState<OrderDetails>({deliveryMethod: "pickup"});
 
   return (
-    <AppContext.Provider value={{ menuItems, setMenuItems, cart, setCart }}>
+    <AppContext.Provider value={{ menuItems, setMenuItems, cart, setCart, orderDetails, setOrderDetails}}>
       {children}
     </AppContext.Provider>
   );
