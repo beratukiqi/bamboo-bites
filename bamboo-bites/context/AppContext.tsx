41337c4721bd073ad231a0371fbe4ea95f037bee
fabiolapoblete <113706603+fabiolapoblete@users.noreply.github.com>
2023-11-22 10:37:17 +0100

    Added order details context to AppContext

--- bamboo-bites/context/AppContext.tsx
+++ bamboo-bites/context/AppContext.tsx
@@ -17,11 +17,17 @@ interface CartItemProps {
   tweaks?: string[] | undefined;
 }
 
+interface OrderDetails {
+  deliveryMethod: string;
+}
+
 interface AppContextType {
   menuItems: MenuItemProps[];
   setMenuItems: Dispatch<SetStateAction<MenuItemProps[]>>;
   cart: CartItemProps[];
   setCart: Dispatch<SetStateAction<CartItemProps[]>>;
+  orderDetails: OrderDetails;
+  setOrderDetails: Dispatch<SetStateAction<OrderDetails>>
 }
 
 const AppContext = createContext({} as AppContextType);
