132cccb5450dc29562ae6ed2f8dbeaecad5c58ec
fabiolapoblete <113706603+fabiolapoblete@users.noreply.github.com>
2023-11-27 13:42:08 +0100

    Created component for table

--- bamboo-bites/pages/orders.tsx
+++ bamboo-bites/pages/orders.tsx
@@ -1,3 +1,4 @@
+import OrderTable from "@/components/OrderTable"
 import { useEffect, useState } from "react"
 
 interface Order {
@@ -24,49 +25,10 @@ const Orders = () => {
 
     useEffect(() => {
         console.log(orders);
-    }, [orders] )
-
-    const styleStatus = (status: string) => {
-        console.log("style", status);
-        
-        switch (status) {
-            case 'pending':
-                return 'pending-status';
-            case 'cooking':
-                return 'cooking-status'; 
-            case 'pick-up':
-                return 'pickUp-status'; 
-            case 'delivery':
-                return 'delivery-status';
-            case 'picked-up':
-                return 'pickUp-status'; 
-            case 'delivered':
-                return 'delivery-status';      
-            default:
-                return ''; 
-        }
-    };
-    
+    }, [orders] )    
 
     return (
-        <table>
-            <thead>
-                <tr>
-                    <th>Order Number</th>
-                    <th>Time</th>
-                    <th>Status</th>
-                </tr>
-            </thead>
-            <tbody>
-                {orders && orders.map((order) => (
-                    <tr key={order.orderNr}>
-                        <td>{order.orderNr}</td>
-                        <td>{order.timeStamp}</td>
-                        <td className={styleStatus(order.status)}>{order.status}</td>
-                    </tr>
-                ))}
-            </tbody>
-        </table>
+        <OrderTable orders={orders}/>
     )
 }
 
