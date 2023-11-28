219ecbc79488c97cfa9878287830e83b0f22d871
LisaRydCarlsson <lisa@rydcarlsson.se>
2023-11-28 15:40:37 +0100

    Created a footer

---
+++ bamboo-bites/components/Footer.tsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import { SvgIcons } from "./SvgIcons";
+
+const Footer = () => {
+	const [isOpen, setIsOpen] = useState(false);
+
+	const toggleFooter = () => {
+		setIsOpen(!isOpen);
+		console.log(isOpen ? "Closed" : "Open");
+	};
+
+	return (
+		<footer className={`footer ${isOpen ? "open" : "closed"}`}>
+			<span className="footer-toggle" onClick={toggleFooter}>
+				{isOpen ? SvgIcons.CaretDownIcon : SvgIcons.CaretUpIcon}
+			</span>
+			<section className="footer-container">
+				{isOpen ? (
+					<section className="footer-left">
+						<h3>Connect with Us Anywhere!</h3>
+						<p>
+							123 SUSHI STREET, <br />
+							BROOKLYN, NY 11234
+						</p>
+						<p>(555) 123-4567</p>
+						<p>order@bamboobites.com</p>
+					</section>
+				) : null}
+				<section className="footer-center">
+					{isOpen ? <h3>Open Daily, 11 am - 11 pm</h3> : null}
+				</section>
+				{isOpen ? (
+					<section className="footer-right">
+						<h3>Taste the Buzz on Social!</h3>
+						<div className="social-icons">
+							{SvgIcons.FacebookIcon}
+							{SvgIcons.InstagramIcon}
+							{SvgIcons.SnapchatIcon}
+							{SvgIcons.TikTokIcon}
+						</div>
+						<p>All images on this website by Freepik</p>
+					</section>
+				) : null}
+			</section>
+		</footer>
+	);
+};
+
+export default Footer;
