5c7a69a8168df0bb368bc5ade8f04a565f4c60d9
fabiolapoblete <113706603+fabiolapoblete@users.noreply.github.com>
2023-12-05 14:19:16 +0100

    Styling for admin page

--- bamboo-bites/pages/admin.tsx
+++ bamboo-bites/pages/admin.tsx
@@ -1,78 +1,201 @@
-import Button from "@/components/Button";
-import OrderList from "@/components/OrderList";
-import OrderModal from "@/components/OrderModal";
-import PageColumn from "@/components/PageColumn";
-import PageHeader from "@/components/PageHeader";
+import OrderTable from "@/components/OrderTable";
 import PageWrapper from "@/components/PageWrapper";
 import { useEffect, useState } from "react";
+import {SvgIcons} from "../components/SvgIcons"
 
-interface OrderDetail {
-  id: string;
-  item: string;
-  price: number;
-  desc: string;
-  imgUrl: string;
-  quantity: number;
-  orderNr: number;
-}
+const navItems = [
+	{
+		name: "Orders",
+    icon: SvgIcons.OrdersIcon
+	},
+	{
+		name: "Statistics",
+    icon: SvgIcons.StatIcon
+	},
+	{
+		name: "Time reports",
+    icon: SvgIcons.TimeReportIcon
+	},
+	{
+		name: "Settings",
+    icon: SvgIcons.SettingsIcon
+	},
+];
 
 interface Order {
-  order: OrderDetail[];
-  orderNr: string;
-  status: string;
+  orderNr: number;
   timeStamp: string;
-  totalPrice: number;
-  deliveryMethod: string;
+  status: string;
 }
 
-const Orders = () => {
-  const [ordersData, setOrdersData] = useState<Order[]>();
+const Admin = () => {
+  const today = new Date();
+  const todaysDate = today.toISOString().split("T")[0] + "T00:00:00";
 
-  // Fetches order data on mount and sets it to state
+  const statusList = [
+    "pending",
+    "cooking",
+    "eat in",
+    "take away",
+    "done",
+  ];
+
+  const [ordersByStatus, setOrdersByStatus] = useState<{
+    [status: string]: Order[];
+  }>({});
+
+  const fetchOrdersByStatus = async (status: string) => {
+    try {
+      const response = await fetch(
+        `https://x1keilhp1a.execute-api.eu-north-1.amazonaws.com/api/filterOrders/${status}?timeStamp=${todaysDate}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const data = await response.json();
+      console.log(status, data.filteredOrders);
+
+      setOrdersByStatus((prevOrders) => ({
+        ...prevOrders,
+        [status]: data.filteredOrders,
+      }));
+    } catch (error) {
+      console.error(error, `Failed to fetch ${status} orders`);
+    }
+  };
+
   useEffect(() => {
-    const fetchOrderData = async () => {
-      try {
-        const res = await fetch(
-          `https://x1keilhp1a.execute-api.eu-north-1.amazonaws.com/api/orders`
-        );
-        const data = await res.json();
-        console.log(data.orders);
-
-        // Update state after data is fetched
-        setOrdersData(data.orders);
-      } catch (error) {
-        console.error("Error fetching order data:", error);
-        // Handle error case
-      }
-    };
-
-    fetchOrderData();
+    statusList.forEach((status) => fetchOrdersByStatus(status));
   }, []);
 
-  const [chosenOrder, setChosenOrder] = useState<Order>();
+  useEffect(() => {
+    console.log(ordersByStatus);
+  }, [ordersByStatus]);
+
+  const [activeOrder, setActiveOrder] = useState(null);
+
+  const openModal = (order: any) => {
+    setActiveOrder(order);
+  };
+
+  const closeModal = () => {
+    setActiveOrder(null);
+  };
 
   return (
-    <PageWrapper id="contact" column>
-      <PageHeader
-        title="Admin"
-        img="https://bamboo-bites-bucket.s3.eu-north-1.amazonaws.com/desktop/contact_desktop_720x1024.png"
-      />
-      <PageColumn title="">
-        {ordersData ? (
-          ordersData.map((order: Order, i: number) => (
-            <>
-              <p onClick={() => setChosenOrder(order)} key={i}>
-                {order.orderNr}
-              </p>
-            </>
-          ))
-        ) : (
-          <p>No orders found</p>
-        )}
-        {chosenOrder && <OrderModal orderItem={chosenOrder} />}
-      </PageColumn>
+    <PageWrapper id="admin">
+      <aside>
+        <nav className="admin-nav">
+          {SvgIcons.LogoIcon}
+          <ul className = "admin-nav__links">
+            {navItems.map((item) => (
+              <article key={item.name} className="admin-nav__items">
+                {item.icon}
+                <li>{item.name}</li>
+              </article>
+            ))}
+          </ul>
+        </nav>
+      </aside>
+      <section className="table-wrapper">
+        {statusList.map((status) => (
+          <OrderTable
+            activeOrder={activeOrder}
+            closeModal={closeModal}
+            openModal={openModal}
+            key={status}
+            status={status}
+            orders={ordersByStatus[status] || []}
+          />
+        ))}
+      </section>
+      <section className="berattest"></section>
     </PageWrapper>
   );
 };
 
-export default Orders;
+export default Admin;
+
+
+
+// import Button from "@/components/Button";
+// import OrderList from "@/components/OrderList";
+// import OrderModal from "@/components/OrderModal";
+// import PageColumn from "@/components/PageColumn";
+// import PageHeader from "@/components/PageHeader";
+// import PageWrapper from "@/components/PageWrapper";
+// import { useEffect, useState } from "react";
+
+// interface OrderDetail {
+//   id: string;
+//   item: string;
+//   price: number;
+//   desc: string;
+//   imgUrl: string;
+//   quantity: number;
+//   orderNr: number;
+// }
+
+// interface Order {
+//   order: OrderDetail[];
+//   orderNr: string;
+//   status: string;
+//   timeStamp: string;
+//   totalPrice: number;
+//   deliveryMethod: string;
+// }
+
+// const Orders = () => {
+//   const [ordersData, setOrdersData] = useState<Order[]>();
+
+//   // Fetches order data on mount and sets it to state
+//   useEffect(() => {
+//     const fetchOrderData = async () => {
+//       try {
+//         const res = await fetch(
+//           `https://x1keilhp1a.execute-api.eu-north-1.amazonaws.com/api/orders`
+//         );
+//         const data = await res.json();
+//         console.log(data.orders);
+
+//         // Update state after data is fetched
+//         setOrdersData(data.orders);
+//       } catch (error) {
+//         console.error("Error fetching order data:", error);
+//         // Handle error case
+//       }
+//     };
+
+//     fetchOrderData();
+//   }, []);
+
+//   const [chosenOrder, setChosenOrder] = useState<Order>();
+
+//   return (
+//     <PageWrapper id="contact" column>
+//       <PageHeader
+//         title="Admin"
+//         img="https://bamboo-bites-bucket.s3.eu-north-1.amazonaws.com/desktop/contact_desktop_720x1024.png"
+//       />
+//       <PageColumn title="">
+//         {ordersData ? (
+//           ordersData.map((order: Order, i: number) => (
+//             <>
+//               <p onClick={() => setChosenOrder(order)} key={i}>
+//                 {order.orderNr}
+//               </p>
+//             </>
+//           ))
+//         ) : (
+//           <p>No orders found</p>
+//         )}
+//         {chosenOrder && <OrderModal orderItem={chosenOrder} />}
+//       </PageColumn>
+//     </PageWrapper>
+//   );
+// };
+
+// export default Orders;
