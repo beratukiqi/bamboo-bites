d932ef36fa98cb3ad2f33df52637d3bf62078993
beratukiqi <berat.u@live.se>
2023-12-11 09:13:00 +0100

    Fixed grid-order for addons

--- bamboo-bites/components/Addons.tsx
+++ bamboo-bites/components/Addons.tsx
@@ -1,53 +1,61 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { SvgIcons } from "./SvgIcons";
 import AppContext from "@/context/AppContext";
 import Image from "next/image";
 
 interface ExtrasProps {
-	id: string;
-	item: string;
-	price: number;
+  data: {
+    id: string;
+    item: string;
+    price: number;
+    imgUrl: string;
+  };
 }
 
 const Addons = ({ data }: ExtrasProps) => {
-	const { cart, setCart } = useContext(AppContext);
+  const { cart, setCart } = useContext(AppContext);
 
-	const addToCart = (extra: any) => {
-		const itemInCart = cart.find((item) => item.id === extra.id);
-		if (itemInCart) {
-			setCart(
-				cart.map((item) =>
-					item.id === extra.id ? { ...item, quantity: item.quantity + 1 } : item
-				)
-			);
-		} else {
-			setCart([...cart, { ...extra, quantity: 1 }]);
-		}
-	};
-	return (
-		<div className="extras-container__wrapper">
-			{data &&
-				data.map((item: any, i: number) => (
-					<article className="extras-container">
-						<img
-							src={item.imgUrl}
-							alt={`Top view image of the dish ${item.item}`}
-							className="extras-container__image"
-						/>
-						<section className="extras-container__info">
-							<h3>{item.item}</h3>
-							<span>
-								<b>$</b>
-								{item.price}
-							</span>
-							<button onClick={() => addToCart(item)}>
-								{SvgIcons.AddIcon}
-							</button>
-						</section>
-					</article>
-				))}
-		</div>
-	);
+  const addToCart = (extra: any) => {
+    const itemInCart = cart.find((item) => item.id === extra.id);
+    if (itemInCart) {
+      setCart(
+        cart.map((item) =>
+          item.id === extra.id ? { ...item, quantity: item.quantity + 1 } : item
+        )
+      );
+    } else {
+      setCart([...cart, { ...extra, quantity: 1 }]);
+    }
+  };
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
+
+  return (
+    <div className="extras-container__wrapper">
+      {data &&
+        data.map((item: any, i: number) => (
+          <article className="extras-container">
+            <img
+              src={item.imgUrl}
+              alt={`Top view image of the dish ${item.item}`}
+              className="extras-container__image"
+            />
+            <section className="extras-container__info">
+              <h3>{item.item}</h3>
+              <span>
+                <b>$</b>
+                {item.price}
+              </span>
+              <button onClick={() => addToCart(item)}>
+                {SvgIcons.AddIcon}
+              </button>
+            </section>
+          </article>
+        ))}
+    </div>
+  );
 };
 
 export default Addons;
